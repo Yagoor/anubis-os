692fc9f19f64f11213b3afd1d66065b1b49c6e45
Yagoor <yago.rosario@hotmail.com.br>
2021-03-17 20:20:04 +0000

    Build all apps job

--- apps/adder/adder.c
+++ apps/adder/adder.c
@@ -5,6 +5,5 @@
 int main()
 {
 	printf("Add %d\n", add(5, 6));
-	printf("Sub %d\n", add(6, 5));
 	return (0);
 }
---
+++ apps/subtractor/subtractor.c
@@ -0,0 +1,9 @@
+#include "sub.h"
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Sub %d\n", sub(6, 5));
+	return (0);
+}
