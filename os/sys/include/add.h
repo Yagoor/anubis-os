e0b4930bbb65ebdee309a0d8d90ee6e46d200b90
Yagoor <yago.rosario@hotmail.com.br>
2021-03-18 16:36:33 +0000

    Doxygen

--- os/sys/include/add.h
+++ os/sys/include/add.h
@@ -1,6 +1,46 @@
-#ifndef ADD_H_
-#define ADD_H_
+/***************************************************************************
+ * Copyright (C) 2021 by Yago Fontoura do Rosario                                   *
+ *                                                                          *
+ * This file is part of Anubis-OS.                                          *
+ *                                                                          *
+ * Permission is hereby granted, free of charge, to any person obtaining    *
+ * a copy of this software and associated documentation files (the          *
+ * "Software"), to deal in the Software without restriction, including      *
+ * without limitation the rights to use, copy, modify, merge, publish,      *
+ * distribute, sublicense, and/or sell copies of the Software, and to       *
+ * permit persons to whom the Software is furnished to do so, subject to    *
+ * the following conditions:                                                *
+ *                                                                          *
+ * The above copyright notice and this permission notice shall be           *
+ * included in all copies or substantial portions of the Software.          *
+ *                                                                          *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          *
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF       *
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                    *
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE   *
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION   *
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION    *
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          *
+ ****************************************************************************/
 
+/**
+ * @file add.h
+ * @author Yago Fontoura do Rosario
+ * @date 9 Sep 2012
+ * @brief File containing an add function
+ *
+ * @see https://en.wikipedia.org/wiki/Addition
+ */
+#ifndef ADD_H_
+#define	ADD_H_
+
+/**
+ * @brief Adds two integer
+ *
+ * @param a integer a
+ * @param b integer b
+ * @return int a + b
+ */
 int add(int a, int b);
 
 #endif /* ADD_H_ */
